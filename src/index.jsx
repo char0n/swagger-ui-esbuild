0fee7590c80301f65de109c6ee642614d762c6c6
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-01-09 09:53:46 +0100

    chore: integrate swagger-ui with React@18 support

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 
-const rootNode = document.getElementById('root');
-ReactDOM.render(<App />, rootNode);
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
